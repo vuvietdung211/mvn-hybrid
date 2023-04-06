98ea0738d78fdd560de9a2f52dc80ae98482f5d1
vietdung <dungvv211@gmail.com>
2023-04-06 23:37:46 +0700

    update multiple environment

--- src/main/java/commons/BasePageFactory.java
+++ src/main/java/commons/BasePageFactory.java
@@ -1,19 +1,15 @@
 package commons;
 
-import java.util.List;
-import java.util.Set;
-
-import org.openqa.selenium.Alert;
-import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.Color;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.List;
+import java.util.Set;
 
 public class BasePageFactory {
 	protected void openPageUrl(WebDriver driver, String pageUrl) {
